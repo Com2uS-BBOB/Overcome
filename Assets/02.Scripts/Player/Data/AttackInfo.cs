29028f14097f5857e83dee6d0924e89325aa910c
mambasa <101706131+kal990574@users.noreply.github.com>
2026-01-19 16:43:06 +0900

    fix: 플레이어 기능 원 상태 복구

--- Assets/02.Scripts/Player/Data/AttackInfo.cs
+++ Assets/02.Scripts/Player/Data/AttackInfo.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 namespace _02.Scripts.Player.Data
 {
@@ -15,7 +16,7 @@ namespace _02.Scripts.Player.Data
     /// <summary>
     /// 공격 정보 (적 공격에서 플레이어 피격 시스템으로 전달)
     /// </summary>
-    [System.Serializable]
+    [Serializable]
     public struct AttackInfo
     {
         public float Damage;
