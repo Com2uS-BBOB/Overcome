1aa48a23e3cb7cd829f6d09c42f63026481e5c09
Omarchoi <omarchoi80@gmail.com>
2026-01-20 18:25:49 +0900

    feat: UI Cooldown 표시 기능 추가 및 UI 위치 및 모양 수정

--- Assets/02.Scripts/UI/InGame/UI_CrescentSkill.cs
+++ Assets/02.Scripts/UI/InGame/UI_CrescentSkill.cs
@@ -1,7 +1,46 @@
+using System.Collections;
 using _02.Scripts.Player.Combat;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_CrescentSkill : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private CrescentSkill _crescentSkill;
+    [SerializeField] private Image _blockImage;
+    [SerializeField] private Image _skillImage;
+    
+    [Header("스킬 색상")]
+    [SerializeField] private Color _skillUnuseColor;
+    [SerializeField] private Color _skillUseColor;
+
+    [SerializeField] private float _resetDuration;
+    Coroutine _resetImageCoroutine;
+    
+    private void Start()
+    {
+        _blockImage.gameObject.SetActive(!_crescentSkill.CanUse);
+        _crescentSkill.OnCrescentEnded += ActivateSkillUsingImage;
+    }
+    
+    private void OnDestroy()
+    {
+        _crescentSkill.OnCrescentEnded -= ActivateSkillUsingImage;
+    }
+
+    private void ActivateSkillUsingImage()
+    {
+        _skillImage.color = _skillUseColor;
+        if (_resetImageCoroutine != null)
+        {
+            StopCoroutine(_resetImageCoroutine);
+        }
+        _resetImageCoroutine = StartCoroutine(ResetImageCoroutine());
+    }
+
+    private IEnumerator ResetImageCoroutine()
+    {
+        yield return new WaitForSeconds(_resetDuration);
+        _skillImage.color = _skillUnuseColor;
+    }
 }
